3251724b1b852c7db36a2c11b317063dfb35c0c1
sinawebgard <webgard@gmail.com>
2018-10-22 00:58:43 +0100

    adding tweet_count

--- userinfo.R
+++ userinfo.R
@@ -1,13 +1,16 @@
 if(!exists("mydata")){
         mydata <- read.csv("data/tweets_persian.csv", stringsAsFactors = FALSE)  
 }
-user.names <- unique(mydata$user_display_name)
+user.names <- unique(mydata$userid)
 a <- length(user.names)
 user.info <- data.frame()
 for (i in 1:a){
-        temp <- mydata[mydata$user_display_name == 
+        temp <- mydata[mydata$userid == 
                                user.names[i], 2:11]
-        user.info <- rbind(user.info, temp[1, ])
+        tweet_count <- length(mydata
+                              [mydata$userid == user.names[i], 1])
+        temp <- cbind(temp[1, ], tweet_count)
+        user.info <- rbind(user.info, temp)
 
 }
-rm(list = c("temp", "a", "i", "user.names", "mydata"))
+rm(list = c("a", "i", "tweet_count", "user.names", "temp"))
